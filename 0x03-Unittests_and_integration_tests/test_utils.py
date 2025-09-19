3b442892a0f8ea2d3db3341243c23fde82c0a093
Maha065 <maha060506@gmail.com>
2025-09-19 12:06:42 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,8 +59,6 @@ class TestGetJson(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-#!/usr/bin/env python3
-"""Unit tests for utils module."""
 
 import unittest
 from parameterized import parameterized
@@ -79,6 +77,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test that access_nested_map returns expected results."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
 
     @parameterized.expand([
         ({}, ("a",)),
