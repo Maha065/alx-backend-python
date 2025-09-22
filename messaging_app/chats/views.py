4ab7c1eb5b74a1b46c0c69d08de7898e2caf81e5
Maha065 <maha060506@gmail.com>
2025-09-22 18:30:22 +0100

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,9 +1,14 @@
 # messaging_app/chats/views.py
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+
 from .permissions import IsParticipantOfConversation
 from .models import Message, Conversation
 from .serializers import MessageSerializer, ConversationSerializer
+from .filters import MessageFilter
+from .pagination import MessagePagination
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -11,7 +16,6 @@ class ConversationViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
-        # Only return conversations the user is part of
         return Conversation.objects.filter(participants=self.request.user)
 
 
@@ -19,13 +23,16 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    pagination_class = MessagePagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_class = MessageFilter
+    ordering_fields = ["timestamp"]  # allow ordering messages by time
+    search_fields = ["content"]  # allow searching in message content
 
     def get_queryset(self):
-        # Only return messages from conversations the user is part of
         return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
-        # Ensure message is tied to a conversation the user belongs to
         conversation = serializer.validated_data.get("conversation")
         if self.request.user in conversation.participants.all():
             serializer.save(sender=self.request.user)
