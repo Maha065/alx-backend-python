5cae338b64a18ab2e8bcca0c39f7d002a5c2e585
Maha065 <maha060506@gmail.com>
2025-10-01 18:26:17 +0100

    Update tests.py

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,28 +1,20 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Message, Notification, MessageHistory
+from .models import Message
 
 
-class UserDeletionTest(TestCase):
+class UnreadMessagesManagerTest(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user(username="alice", password="pass123")
         self.user2 = User.objects.create_user(username="bob", password="pass123")
 
-    def test_user_deletion_cleans_related_data(self):
-        # Create a message and notification
-        msg = Message.objects.create(sender=self.user1, receiver=self.user2, content="Hi Bob")
-        msg.content = "Edited Hi Bob"
-        msg.save()  # creates MessageHistory
+    def test_unread_messages_for_user(self):
+        # One unread message
+        msg1 = Message.objects.create(sender=self.user1, receiver=self.user2, content="Hello Bob!", read=False)
+        # One read message
+        msg2 = Message.objects.create(sender=self.user1, receiver=self.user2, content="This is read", read=True)
 
-        # Ensure objects exist
-        self.assertEqual(Message.objects.count(), 1)
-        self.assertEqual(Notification.objects.count(), 1)
-        self.assertEqual(MessageHistory.objects.count(), 1)
+        unread_msgs = Message.unread.for_user(self.user2)
 
-        # Delete user1
-        self.user1.delete()
-
-        # Related data should be deleted
-        self.assertEqual(Message.objects.count(), 0)
-        self.assertEqual(Notification.objects.count(), 0)
-        self.assertEqual(MessageHistory.objects.count(), 0)
+        self.assertEqual(unread_msgs.count(), 1)
+        self.assertEqual(unread_msgs.first(), msg1)
