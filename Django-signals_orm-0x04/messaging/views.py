17dfc8f2b3f385b71044da89c615b1d551ffc557
Maha065 <maha060506@gmail.com>
2025-10-01 18:25:54 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,15 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model, logout
-from django.http import HttpResponse
-
-User = get_user_model()
+from .models import Message
 
 
 @login_required
-def delete_user(request):
-    """Allow the logged-in user to delete their account"""
-    user = request.user
-    logout(request)  # log out before deletion
-    user.delete()
-    return HttpResponse("Your account and related data have been deleted successfully.")
+def inbox(request):
+    """Display only unread messages for the logged-in user"""
+    unread_messages = Message.unread.for_user(request.user)
+    return render(request, "messaging/inbox.html", {"unread_messages": unread_messages})
