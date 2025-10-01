ce81e9dcef78d58ae0ac31f9b63df50e38f8ecd7
Maha065 <maha060506@gmail.com>
2025-10-01 18:20:12 +0100

    Update admin.py

--- Django-signals_orm-0x04/ messaging/admin.py
+++ Django-signals_orm-0x04/ messaging/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ("id", "sender", "receiver", "timestamp")
+    list_display = ("id", "sender", "receiver", "timestamp", "edited")
     search_fields = ("sender__username", "receiver__username", "content")
 
 
@@ -13,3 +13,9 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "message", "is_read", "created_at")
     list_filter = ("is_read", "created_at")
     search_fields = ("user__username",)
+
+
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "message", "old_content", "edited_at")
+    search_fields = ("message__content",)
