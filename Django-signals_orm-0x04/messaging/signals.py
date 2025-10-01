159cb4988d9067c83cac79e995dd184eea4552e9
Maha065 <maha060506@gmail.com>
2025-10-01 18:22:15 +0100

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,30 +1,21 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import Message, Notification, MessageHistory
 
-
-@receiver(post_save, sender=Message)
-def create_notification(sender, instance, created, **kwargs):
-    """Create a Notification whenever a new Message is created"""
-    if created:
-        Notification.objects.create(
-            user=instance.receiver,
-            message=instance
-        )
+User = get_user_model()
 
 
-@receiver(pre_save, sender=Message)
-def log_message_edit(sender, instance, **kwargs):
-    """Log old content before a message is updated"""
-    if instance.pk:  # means message already exists
-        try:
-            old_instance = Message.objects.get(pk=instance.pk)
-        except Message.DoesNotExist:
-            return
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    """Clean up related data when a user account is deleted"""
+    # Delete all messages sent/received by the user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
 
-        if old_instance.content != instance.content:  # content is changing
-            MessageHistory.objects.create(
-                message=instance,
-                old_content=old_instance.content
-            )
-            instance.edited = True  # mark as edited
+    # Delete notifications belonging to the user
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message history entries for messages linked to this user
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    MessageHistory.objects.filter(message__receiver=instance).delete()
